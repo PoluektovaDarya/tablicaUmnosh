17a24e50a467057fc63e70fb7722f343849a2de8
PoluektovaDarya <123874713+PoluektovaDarya@users.noreply.github.com>
2023-06-08 11:43:55 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //объявляем и инициализируем массив
-        int m = 1,result,element = 0,s = array[element], nunberUmnozh = 1;
-
-        System.out.println("Умножение на " + nunberUmnozh);
-        for (int i = 1; i < 100; i++) {
-            result = s * m;
-            System.out.println( s + " * " + m + " = " + result);
-            m++;
-            if (m > 10) {
-                nunberUmnozh++;
-                System.out.println("Умножение на " + nunberUmnozh);
-                m = 1;
-                element++;
-                s = array[element];
+        for(byte i = 0; i <= 10; i++){
+            for(byte g = 0; g <= 10; g++){
+                System.out.printf("%d * %d = %d \n",i,g,(i*g));
             }
+            System.out.println("=========");
         }
     }
 }
